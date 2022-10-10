ac992c2f0e8c3ac3f50bd6dcf48f2b8f22821b92
Thiago Beleren <82111604+ThiagoBeleren@users.noreply.github.com>
2022-10-10 18:42:54 -0300

    quase terminando o site

---
+++ js/projectslinks.js
@@ -0,0 +1,20 @@
+function github (){
+    window.location.href="https://github.com/ThiagoBeleren"
+}
+function linkedin () {
+    window.location.href = "https://www.linkedin.com/in/thiago-beleren-923b6124b/"
+}
+function gmail (){
+    window.location.href = 'https://www.instagram.com/beleren_thiago/'
+}
+function discord (){
+    window.location.href = 'https://discord.gg/HgRgUbCRrx'
+}
+function replit (){
+    window.location.href = 'https://replit.com/@ThiagoBeleren'
+}
+
+function project1 (){
+    document.getElementById('box1')
+    window.location.href = 'https://github.com/ThiagoBeleren/mylinktree'
+}
